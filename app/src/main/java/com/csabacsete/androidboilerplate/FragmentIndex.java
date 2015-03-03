f2cb2d858ecf2159665a0870fd5302fad39fb3fb
Csaba Csete <csaba.csete@gmail.com>
2015-03-03 22:25:42 +0200

    G+ sign in, main nav drawer, etc

---
+++ app/src/main/java/com/csabacsete/androidboilerplate/FragmentIndex.java
@@ -0,0 +1,34 @@
+package com.csabacsete.androidboilerplate;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentIndex extends Fragment {
+
+
+    public FragmentIndex() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TextView text = new TextView(this.getActivity());
+        text.setText("Section");
+        text.setGravity(Gravity.CENTER);
+        return text;
+    }
+
+
+}
---
+++ app/src/main/java/com/csabacsete/androidboilerplate/MainActivity.java
@@ -0,0 +1,176 @@
+package com.csabacsete.androidboilerplate;
+
+import android.content.Intent;
+import android.content.IntentSender;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+
+import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
+import it.neokree.materialnavigationdrawer.elements.MaterialSection;
+import it.neokree.materialnavigationdrawer.elements.listeners.MaterialSectionListener;
+
+
+public class MainActivity extends MaterialNavigationDrawer implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    /**
+     * Request code used to invoke sign in user interactions.
+     */
+    private static final int RC_SIGN_IN = 0;
+    private static final String TAG = "LoginActivity";
+
+    /**
+     * Client used to interact with Google APIs.
+     */
+    private GoogleApiClient mGoogleApiClient;
+
+    /**
+     * A flag indicating that a PendingIntent is in progress and prevents
+     * us from starting further intents.
+     */
+    private boolean mIntentInProgress;
+
+    /**
+     * Track whether the sign-in button has been clicked so that we know to resolve
+     * all issues preventing sign-in without waiting.
+     */
+    private boolean mSignInClicked;
+
+    /**
+     * Store the connection result from onConnectionFailed callbacks so that we can
+     * resolve them when the user clicks sign-in.
+     */
+    private ConnectionResult mConnectionResult;
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this).build();
+        ImageLoader.getInstance().init(config);
+
+        // create sections
+        this.addSection(newSection("Recycle GridView", R.mipmap.ic_grid, new FragmentIndex()).setSectionColor(Color.parseColor("#F44336")));
+        this.addSection(newSection("ViewPager", R.mipmap.ic_swipe, new FragmentIndex()).setSectionColor(Color.parseColor("#9C27B0")));
+        this.addSection(newSection("Google Maps", R.mipmap.ic_map, new FragmentIndex()).setSectionColor(Color.parseColor("#2196F3")));
+
+        // create bottom section
+        this.addBottomSection(newSection("Preferences", R.mipmap.ic_preferences, new Intent(this, SettingsActivity.class)));
+        this.addBottomSection(newSection("Logout", R.mipmap.ic_logout, new MaterialSectionListener() {
+            @Override
+            public void onClick(MaterialSection materialSection) {
+                if (mGoogleApiClient.isConnected()) {
+                    Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+                    mGoogleApiClient.disconnect();
+                    mGoogleApiClient.connect();
+                }
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        }));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(Plus.SCOPE_PLUS_LOGIN)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "onConnected");
+        mSignInClicked = false;
+        Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
+
+        if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
+            Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+            String email = Plus.AccountApi.getAccountName(mGoogleApiClient);
+
+            // set header data
+            setUsername(currentPerson.getDisplayName());
+            setUserEmail(email);
+            ImageLoader.getInstance().loadImage(currentPerson.getCover().getCoverPhoto().getUrl(),
+                    new SimpleImageLoadingListener() {
+                        @Override
+                        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                            BitmapDrawable d = new BitmapDrawable(loadedImage);
+                            setDrawerHeaderImage(d);
+                        }
+                    });
+            ImageLoader.getInstance().loadImage(currentPerson.getImage().getUrl(),
+                    new SimpleImageLoadingListener() {
+                        @Override
+                        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                            RoundedBitmapDrawable d = RoundedBitmapDrawableFactory.create(getResources(), loadedImage);
+                            d.setCornerRadius(Math.max(loadedImage.getWidth(), loadedImage.getHeight()) / 2.0f);
+                            setFirstAccountPhoto(d);
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "onConnectionSuspended");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(TAG, "onConnectionFailed");
+        if (!mIntentInProgress) {
+            // Store the ConnectionResult so that we can use it later when the user clicks
+            // 'sign-in'.
+            mConnectionResult = connectionResult;
+
+            if (mSignInClicked) {
+                // The user has already clicked 'sign-in' so we attempt to resolve all
+                // errors until the user is signed in, or they cancel.
+                resolveSignInError();
+            }
+        }
+    }
+
+    /* A helper method to resolve the current ConnectionResult error. */
+    private void resolveSignInError() {
+        if (mConnectionResult.hasResolution()) {
+            try {
+                mIntentInProgress = true;
+                startIntentSenderForResult(mConnectionResult.getResolution().getIntentSender(),
+                        RC_SIGN_IN, null, 0, 0, 0);
+            } catch (IntentSender.SendIntentException e) {
+                // The intent was canceled before it was sent.  Return to the default
+                // state and attempt to connect to get an updated ConnectionResult.
+                mIntentInProgress = false;
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+}
